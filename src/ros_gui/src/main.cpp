bcf5c43aea43787d6ea089ec77d6c9e4fb410700
zhangchenxu <zhangchenxuv@163.com>
2026-04-08 00:48:23 +0800

    feat: 初始化 ROS2 + Qt GUI 项目框架

--- src/ros_gui/src/main.cpp
+++ src/ros_gui/src/main.cpp
@@ -1,11 +1,19 @@
 #include "mainwindow.h"
 #include <QApplication>
 #include <rclcpp/rclcpp.hpp>
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
     QApplication a(argc, argv);
+
+    QFile file(":/resource/style.qss");
+    if (file.open(QFile::ReadOnly))
+    {
+        a.setStyleSheet(file.readAll());
+        file.close();
+    }
 
     MainWindow w;
     w.show();
